abbe95d1708a578d04d616f9e9d08d3adb163723
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-09-07 11:54:54 -0500

    started landing.js random feature

---
+++ assets/js/landing.js
@@ -0,0 +1,7 @@
+var randomCocktailImgEl = $("#random-cocktail-img");
+var randomCocktailRecipeA = $("#random-cocktail-rec");
+
+var randomFoodImgEl = $("#random-food-img");
+var randomFoodRecipeA = $("#random-food-rec");
+
+
