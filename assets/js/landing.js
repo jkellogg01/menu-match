ade134e802213268cc65d6de468121bad1277b83
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-09-07 14:12:27 -0500

    random cocktail image on landing page

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -4,19 +4,22 @@ var randomCocktailRecipeA = $("#random-cocktail-rec");
 var randomFoodImgEl = $("#random-food-img");
 var randomFoodRecipeA = $("#random-food-rec");
 
+
+
+
+
 function getRandomFood() {
   $.ajax({
     url: mealDBEndpoint + mealDBExtensions.random,
     method: "GET",
   }).then(function (data) {
-    console.log(data.meals);
 
     randomFoodImgEl.attr("src", data.meals[0].strMealThumb);
     randomFoodRecipeA.attr("href", )
   });
 };
 
-getRandomFood();
+
 
 
 function getRandomCocktail () {
@@ -26,7 +29,10 @@ function getRandomCocktail () {
   }) .then(function(data){
     console.log (data);
 
+    randomCocktailImgEl.attr("src", data.drinks[0].strDrinkThumb);
+    randomCocktailRecipeA.attr("href", )
+  });
+};
 
-  }
-  )
-}
+getRandomCocktail();
+getRandomFood();
