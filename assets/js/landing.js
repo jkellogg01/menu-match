e4042b6ea207c8d70c34767da47ebda25d86dd72
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-09-07 13:23:32 -0500

    added random meal gneration on landing page food card

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -4,7 +4,29 @@ var randomCocktailRecipeA = $("#random-cocktail-rec");
 var randomFoodImgEl = $("#random-food-img");
 var randomFoodRecipeA = $("#random-food-rec");
 
+function getRandomFood() {
+  $.ajax({
+    url: mealDBEndpoint + mealDBExtensions.random,
+    method: "GET",
+  }).then(function (data) {
+    console.log(data.meals);
 
-var mealDBEndpoint = "https://www.themealdb.com/api/json/v1/1/";
+    randomFoodImgEl.attr("src", data.meals[0].strMealThumb);
+    randomFoodRecipeA.attr("href", )
+  });
+};
 
-var cocktailDBEndpoint = "https://www.thecocktaildb.com/api/json/v1/1/";
+getRandomFood();
+
+
+function getRandomCocktail () {
+  $.ajax ({
+    url: cocktailDBEndpoint + cocktailDBExtensions.random,
+    method: "GET",
+  }) .then(function(data){
+    console.log (data);
+
+
+  }
+  )
+}
