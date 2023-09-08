fb595a9ffbc526e31c44ccd6884d058e16754650
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-09-08 09:57:01 -0600

    slight refactor for displaying saved recipes

--- assets/js/saved-recipes-nav.js
+++ assets/js/saved-recipes-nav.js
@@ -1,7 +1,8 @@
 const savedRecipesNavEl = $("#saved-recipes");
+const recipeDisplayPath = "./display-recipes.html";
 
 savedRecipesNavEl.on("click", () => {
   let savedRecipes = JSON.parse(localStorage.getItem("savedRecipes"));
   localStorage.setItem("displayRecipes", JSON.stringify(savedRecipes));
-  location.replace("./display-recipes.html");
+  $(location).attr("href", recipeDisplayPath);
 });
