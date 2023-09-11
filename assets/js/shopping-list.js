73ff888a59cb8e2bbd21632395df778cf4581702
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2023-09-11 10:41:11 -0600

    updating js

--- assets/js/shopping-list.js
+++ assets/js/shopping-list.js
@@ -1,7 +1,7 @@
 // ================================== localStorage ==================================
 // =========retrieving data=========
 const userData = JSON.parse(localStorage.getItem("shoppingList"));
-console.log(userData);
+// console.log(userData);
 //=========retrieving data end=========
 
 //=========displaying data=========
@@ -14,14 +14,22 @@ function renderShoppingList() {
   userData.forEach((value) => {
     let listName = value;
     const shoppingList = $('<li class="list-group-item">');
+    //
+    // const RemoveBtn = $('<button class="btn btn-danger">');
+    const RemoveBtn = $('<button class="btn btn-danger">');
+    RemoveBtn.text("X");
+    RemoveBtn.on("click", RemoveList);
 
     shoppingList.text(listName);
     shoppingListContainer.append(shoppingList);
+    //
+    shoppingList.append(RemoveBtn);
   });
 }
 renderShoppingList();
 // =========displaying data end=========
 // ===============================================================================
+//sortable
 
 $(function () {
   $("#recipe-list").sortable({
@@ -33,3 +41,13 @@ $(function () {
     },
   });
 });
+
+$(".btn").empty();
+// // remove list item
+// function RemoveList(value) {
+//   let listName = $(value.target);
+//   console.log(userData);
+
+//   localStorage.setItem("shoppingList", JSON.stringify(userData));
+//   renderShoppingList();
+// }
